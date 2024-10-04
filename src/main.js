61f0e68f548dad441c24fcc6797a9da8408de669
Matheus Arakaki <arakakimath@gmail.com>
2024-10-04 15:28:04 -0400

    showing times available according to current time

--- src/main.js
+++ src/main.js
@@ -8,5 +8,9 @@ import "./styles/global.css"
 import "./styles/form.css"
 import "./styles/schedule.css"
 
+// JS
+import "./modules/form/submit.js"
+import "./modules/page-load.js"
+
 import dayjs from "dayjs"
-console.log(dayjs().format("DD/MM HH:mm"))
+//console.log(dayjs().format("DD/MM HH:mm"))
---
+++ src/modules/form/submit.js
@@ -0,0 +1,19 @@
+import dayjs from "dayjs"
+
+const form = document.querySelector("form")
+const selectedDate = document.getElementById("date")
+
+// Carrega a data atual
+const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
+
+// Data atual para o input
+selectedDate.value = inputToday 
+
+// Define a data mínima como sendo a data atual.
+selectedDate.min = inputToday
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  console.log("Enviado!")
+}
