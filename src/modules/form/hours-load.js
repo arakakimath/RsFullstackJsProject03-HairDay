96a56e2c456331f9131bd1fa66cc64393f3429ec
Matheus Arakaki <arakakimath@gmail.com>
2024-10-04 20:47:12 -0400

    Setting unavailable the already scheduled times

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -6,9 +6,12 @@ import { openingHours } from "../../utils/opening-hours";
 
 const hours = document.getElementById("hours")
 
-export function hoursLoad({ date }){
+export function hoursLoad({ date, dailySchedules }){
   // Limpa a lista de horários.
   hours.innerHTML = ""
+
+  const unavailableHours = dailySchedules.map( schedule => dayjs(schedule.when).format("HH:mm"))
+
   const opening = openingHours.map((hour) => {
     // Recupera somente a hora.
     const [scheduleHour] = hour.split(":")
@@ -16,9 +19,11 @@ export function hoursLoad({ date }){
     // Adiciona a hora na date e verifica se está no passado.
     const isHourPast = dayjs(date).add(scheduleHour, "hour").isBefore(dayjs())
     
+    const available = !unavailableHours.includes(hour) && !isHourPast
+
     return {
       hour,
-      available: !isHourPast,
+      available,
     }
   })
   
